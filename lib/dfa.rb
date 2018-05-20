72cc2451749e0e2f96004aef9cf878100b23ef37
Yulia Oletskaya <yulia.oletskaya@gmail.com>
2018-05-20 17:36:59 +0300

    Initial commit

---
+++ lib/dfa.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'dfa/version'
+
+module Dfa
+  # Your code goes here...
+end
---
+++ lib/dfa/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Dfa
+  VERSION = '0.0.0'
+end
